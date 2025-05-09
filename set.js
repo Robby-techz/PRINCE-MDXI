f2a795983e670bbcbf2373e5ec59aecd600f7cae
Robby-techz <robertodurogyimah2008@gmail.com>
2025-05-09 09:05:56 +0000

    .

--- set.js
+++ set.js
@@ -8,16 +8,16 @@ const fs = require('fs'),
 
  
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add sess Id here espwcially when deploying on panels else use app.json and .env file...
+SESSION_ID: process.env.SESSION_ID || "PRINCE-MDX~eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiS0M1a0J5b0QxMndhdkFHVmpZSDNJeGNOOWdiUm91OXgvR3k0NW8wUnIycz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiSElremVDR1FaNFBtQzZPRU0vbmVUeWxlbVVoYVFmYjBlcHowbGQ1enlCaz0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJJRk5hMEFUejVlSU9XU29LbzBZdFNqVFlJTkoyYmtOaUtLM25pUnZUY1VrPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJMQmw5dDdYbzRmd0VYekIxWmZMZnFxV3NqWkpsNFRaMnROMHg1TjlzZjBZPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlFJM2Vqdmp3Qk4zekRnb3pqOGRnUDBOZ3E0TmdEdWJpTTdyamFkMXZIbDA9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Ii96QzFvVk4wNHRQRTJldUNIRXk0eGFaaHpQWDkyNUFFOWN3SWhDbU9raVE9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiNEU1RUFhQi9JeEpDRC9hVmowUjlRRjNuQlo4dlhZdXIzMjB0dUJ5T3dVMD0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoibi85WG9MUHg0akVrclEyeFlxSG5hNFgrNVdraTFhVUEvc2Z3Q1ZpeDAyYz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImovQmpua3pPTXFKRWFZeEI4R05UQUdZM2xYMDRkTG0zU2xKTElXTTdXbUp3a29tNWpidUlIS2VWTysrVUNyUnhpeE4rbndoeHJmZGZUU092L2lxQWdRPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTc1LCJhZHZTZWNyZXRLZXkiOiI4N3JUUmw1eTFIUUFxS3RGZDU1M3NQNEJDU0RWckZ5Uzc3MjUrMUc1Tzl3PSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJ4dndPY0R6SlJpYU1NU0ZNQTBqaHpnIiwicGhvbmVJZCI6Ijc5NmViMGE1LWE3YTctNDc1YS05YjU1LWE0N2UwMmYyMzVmYyIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJFZEt1dzBCK21RQVlMV2tvM1F3VGthZFdVOWc9In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiVEh3cnAwOG5xQlljWHdXUlRVd0dNeEZpUFNvPSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6IldMVFM3NkwxIiwibWUiOnsiaWQiOiIyMzMyMDMxODk4NTY6MjJAcy53aGF0c2FwcC5uZXQifSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ1B2QzNyRUZFS21IOThBR0dBRWdBQ2dBIiwiYWNjb3VudFNpZ25hdHVyZUtleSI6Ilk1ZDNYYjR0Z1BSdExBZkduRkJlRUc3NmdBbm5nRngvSG42Q3haTkpvbFU9IiwiYWNjb3VudFNpZ25hdHVyZSI6Ikt5TUdVRFBBOFN1endnM011ZnZPcVhuM3hxMHJ2Y2E2NTF3ci9WMzhNZUVBTTgwQ3lheU9iOXdydU1pZzRQYWFOSlJGczJHNnAveTJqNSs2aGxacUNRPT0iLCJkZXZpY2VTaWduYXR1cmUiOiJocFdydzlxZ1ppTVNWWW5CcTN1c2pvZWxNcUx4Nkh4QmRJVEFYZEpyWUhpdmw5c0NFbElaTW94NW9RTTllcW9kN1NVK1FwNFVKcVR0K3EwQTA5RWZqZz09In0sInNpZ25hbElkZW50aXRpZXMiOlt7ImlkZW50aWZpZXIiOnsibmFtZSI6IjIzMzIwMzE4OTg1NjoyMkBzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJXT1hkMTIrTFlEMGJTd0h4cHhRWGhCdStvQUo1NEJjZng1K2dzV1RTYUpWIn19XSwicGxhdGZvcm0iOiJhbmRyb2lkIiwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzQ2NzgxMTExLCJteUFwcFN0YXRlS2V5SWQiOiJBQUFBQUduQSJ9", // Add sess Id here espwcially when deploying on panels else use app.json and .env file...
 SUDO_NUMBERS: process.env.SUDO_NUMBERS || "", //Add multiple Numbers with Country Codes without (+) Separated by Comma...
 ANTI_DELETE: process.env.ANTI_DELETE || "inboxonly", // can be set to inboxonly/allchats/true/false
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-AUTO_LIKE_STATUS: process.env.AUTO_LIKE_STATUS || "true",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
+AUTO_LIKE_STATUS: process.env.AUTO_LIKE_STATUS || "false",
 AUTO_LIKE_EMOJIS: process.env.AUTO_LIKE_EMOJIS || "💛,❤️,💜,🤍,💙", //Input Yours Custom...Can be one Emoji or Multiple Emojis Separated by Commas
 AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "false",
 STATUS_REPLY_MSG: process.env.STATUS_REPLY_MSG || "✅️ Status Viewed by prince-mdx", // // Input Yours custom...
 MODE: process.env.MODE || "public", // Put private or public or inbox or groups
-OWNER_NUMBER: process.env.OWNER_NUMBER || "237677224245", // Only 1 owner Number Here, others Add to sudo numbers...
+OWNER_NUMBER: process.env.OWNER_NUMBER || "233203189856", // Only 1 owner Number Here, others Add to sudo numbers...
 OWNER_NAME: process.env.OWNER_NAME || "𝐏𝐑𝐈𝐍𝐂𝐄", // Input Yours custom...(Maintain font for Flow)
 PACK_AUTHOR: process.env.PACK_AUTHOR || "𝐏𝐑𝐈𝐍𝐂𝐄 𝐓𝐄𝐂𝐇", // Added // Input Yours custom...
 PACK_NAME: process.env.PACK_NAME || "𝐏𝐑𝐈𝐍𝐂𝐄 𝐓𝐄𝐂𝐇", // Added // Input Yours custom...
@@ -25,14 +25,14 @@ PREFIX: process.env.PREFIX || ".",
 VERSION: process.env.VERSION || "3.0.0",
 ANTILINK: process.env.ANTILINK || "false", //  Enter true to kick automatically or delete to delete without kicking or warn to warn before kicking
 ANTICALL: process.env.ANTICALL || "false",
-ANTIBAD: process.env.ANTIBAD || "false",
+ANTIBAD: process.env.ANTIBAD || "true",
 BAD_WORDS: process.env.BAD_WORDS || "fuck, pussy, anus, idiot", // Add Yours Separated by Comma(will be deleted if ANTIBAD is set to true)
 ANTICALL_MSG: process.env.ANTICALL_MSG || "*_📞 Auto Call Reject Mode Active. 📵 No Calls Allowed!_*",
 AUTO_REACT: process.env.AUTO_REACT || "false",
 BOT_NAME: process.env.BOT_NAME || "𝐏𝐑𝐈𝐍𝐂𝐄 𝐌𝐃𝐗", //  don't change 
 BOT_PIC: process.env.BOT_PIC || "https://cdn.giftedtech.web.id/file/MrbYl.jpg", //  don't change 
 AUTO_AUDIO: process.env.AUTO_AUDIO || "false",
-AUTO_BIO: process.env.AUTO_BIO || "false",
+AUTO_BIO: process.env.AUTO_BIO || "true",
 AUTO_BIO_QUOTE: process.env.AUTO_BIO_QUOTE || " powered by prince",
 CHAT_BOT: process.env.CHAT_BOT || "false", // Put value to true to enablle for all chats only or inbox to ebanle in pm chats only or groups to enable in groups only else false
 WELCOME: process.env.WELCOME || "false",
